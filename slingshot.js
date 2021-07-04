dfb2b62ca27a9f78290838032ed8730c5d41ddbe
Naman127 <76678038+Naman127@users.noreply.github.com>
2021-07-03 21:32:44 -0700

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,27 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB:pointB,
+            length:50,
+            stiffness:0.04
+        }
+        this.sling = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world,this.sling);
+    }
+    fly(){
+        this.sling.bodyA=null
+    }
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(3.5);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    attach(body){
+        this.sling.bodyA=body;
+    }
+}
